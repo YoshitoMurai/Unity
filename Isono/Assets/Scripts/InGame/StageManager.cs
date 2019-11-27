952712a53181b5114d82540f893cdaa6a139457f
diaburo <diaburo.pc@outlook.jp>
2019-11-28 00:08:32 +0900

    [追加]StageManager作成.

---
+++ Isono/Assets/Scripts/InGame/StageManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Connect.InGame
+{
+    public class StageManager : MonoBehaviour
+    {
+        public void Reset()
+        {
+            var linkCubeObj = GameObject.Find("Link_Cube");
+            if(linkCubeObj != null)
+            {
+                _linkCube = linkCubeObj.GetComponent<Transform>();
+            }
+        }
+
+        [SerializeField] Transform _linkCube;
+
+    }
+}
