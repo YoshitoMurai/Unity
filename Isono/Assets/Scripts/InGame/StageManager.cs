a60ee4d63f2858dae5de76b1c509668567530421
diaburo <diaburo.pc@outlook.jp>
2019-11-28 01:22:21 +0900

    [追加]ContextMenuでアセットを作成できるように.

--- Isono/Assets/Scripts/InGame/StageManager.cs
+++ Isono/Assets/Scripts/InGame/StageManager.cs
@@ -16,6 +16,27 @@ namespace Connect.InGame
         }
 
         [SerializeField] Transform _linkCube;
+        [SerializeField] int _stageNum;
 
+#if UNITY_EDITOR
+        [ContextMenu("SaveAsset")]
+        void saveAsset()
+        {
+            var asset = StageDataSet.LoadForEditor(_stageNum);
+            asset.Clear();
+
+            var cubeTransformArray = _linkCube.GetComponentInChildren<Transform>();
+            foreach (Transform cubeTransform in cubeTransformArray)
+            {
+                Debug.Log(cubeTransform.name);
+
+                asset.Add(cubeTransform.localPosition);
+            }
+
+            asset.SetDirtyForEditor();
+            Debug.Log(asset.name + "の生成に成功");
+
+        }
+#endif
     }
 }
