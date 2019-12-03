922bd11c460c4c5612e1191a22f715a0c9f61adb
Yoshito Murai <47.yoshiyoshi@gmail.com>
2019-12-04 00:38:38 +0900

    コミット漏れ

---
+++ Isono/Assets/Scripts/UI/Common/Fade.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Connect
+{
+    public class Fade : MonoBehaviour
+    {
+        public void FadeOut(CanvasGroup canvasGroup, float duration,  GraphicRaycaster gr)
+        {
+            gr.enabled = true;
+            canvasGroup.DOFade(0.0F, duration).
+                OnComplete(() =>
+                {
+                    canvasGroup.blocksRaycasts = false;
+                    gr.enabled = false;
+                });
+        }
+
+        public void FadeIn(CanvasGroup canvasGroup, float duration, GraphicRaycaster gr)
+        {
+            gr.enabled = true;
+            canvasGroup.DOFade(1.0F, duration).OnComplete(() =>
+            {
+                canvasGroup.blocksRaycasts = true;
+                gr.enabled = false;
+            });
+        }
+    }
+}
