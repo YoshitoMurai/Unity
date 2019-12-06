9f7c7cf82ce46db8ab8befd44ef546cd506c5d21
Yoshito Murai <47.yoshiyoshi@gmail.com>
2019-12-07 00:56:10 +0900

    Added Skin.

---
+++ Isono/Assets/Resources/Shaders/Skin/Skin1.hlsl
@@ -0,0 +1,21 @@
+float hex(float2 p)
+{
+	p.x *= 0.57735;
+	p = abs((fmod(p, 0.8) - 0.4));
+	return abs(max(p.x + p.x, p.y) - 0.5);
+}
+
+void Skin1_float(in float2 uv, in float2 res,in float time, out float3 col)
+{
+	float2 pos = uv.xy;
+	float2 p = 40.0f * pos;
+	float s = sin(dot(p, p) / -64. + time);
+	//col = float3(s, s, s);
+	s = pow(abs(s), 0.5) * sign(s);
+	float  r = .35 + .25 * s;
+	float t =0.8;
+	p =  mul(p,float2x2(cos(t), -sin(t), sin(t), cos(t)));
+	float f = hex(p*r)*0.5+s;
+	col = float3(f, f, f);
+	
+}
