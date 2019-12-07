760f36e7d9f6ff8717570d1e581f109917259d71
Yoshito Murai <47.yoshiyoshi@gmail.com>
2019-12-08 01:08:16 +0900

    Fixed Skin Change

--- Isono/Assets/Scripts/UI/Ingame/IngameView.cs
+++ Isono/Assets/Scripts/UI/Ingame/IngameView.cs
@@ -24,6 +24,8 @@ namespace Connect.InGame.UI
         [SerializeField] private Button _clearbg = default;
         public IObservable<Unit> OnClickClear => _clearbg.OnClickAsObservable();
 
+        public IObservable<int> OnClickSkin;
+
         [SerializeField] private GraphicRaycaster _graphicRaycaster = default;
 
         // Start is called before the first frame update
@@ -34,12 +36,22 @@ namespace Connect.InGame.UI
             _skinChangeButton.OnClickAsObservable().Subscribe(_ => _skinChangeDialog.SetActive(true)).AddTo(gameObject);
             _skinBackButton.OnClickAsObservable().Subscribe(_ => _skinChangeDialog.SetActive(false)).AddTo(gameObject);
             _unlockButton.OnClickAsObservable().Subscribe(_ => AdvertiseManager.Instance.ShowMovieAds());
+            
+            OnClickSkin = _skinButton[0].OnClickAsObservable().Select(_=>0).Merge(
+                _skinButton[1].OnClickAsObservable().Select(_ => 1),
+                _skinButton[2].OnClickAsObservable().Select(_ => 2),
+                _skinButton[3].OnClickAsObservable().Select(_ => 3),
+                _skinButton[4].OnClickAsObservable().Select(_ => 4),
+                _skinButton[5].OnClickAsObservable().Select(_ => 5),
+                _skinButton[6].OnClickAsObservable().Select(_ => 6),
+                _skinButton[7].OnClickAsObservable().Select(_ => 7),
+                _skinButton[8].OnClickAsObservable().Select(_ => 8),
+                _skinButton[9].OnClickAsObservable().Select(_ => 9),
+                _skinButton[10].OnClickAsObservable().Select(_ => 10),
+                _skinButton[11].OnClickAsObservable().Select(_ => 11)
+                );
+            
 
-            for (int i = 0; i < _skinButton.Length; i++)
-            {
-                var index = i;
-                _skinButton[index].OnClickAsObservable().Subscribe(_ => Debug.Log(index));
-            }
             _stageText.text = $"STAGE {stageNum}";
 
             _clearbg.GetComponent<CanvasGroup>().alpha = 0;
